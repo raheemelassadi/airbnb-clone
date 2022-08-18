ae9854b9390987e733e7c061be10eb5a2519350d
raheemelassadi <raheemelassadi1@gmail.com>
2022-08-17 17:24:03 -0700

    prop rearrangement

--- App.js
+++ App.js
@@ -10,16 +10,10 @@ export default function App() {
         return (
             <Card 
                 key={item.id}
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-                openSpots={item.openSpots}
+                item={item}
             />
         )
-    })        
+    })
     
     return (
         <div>
